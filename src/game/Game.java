76244e107056f798f0a9243687738768e8864604
Kal-MH <rkfalgus713@gmail.com>
2024-07-02 08:41:36 +0900

    feature: Pins클래스 수정 및 Game 동작 확인 코드 작성

--- src/game/Game.java
+++ src/game/Game.java
@@ -9,10 +9,13 @@ import java.util.ArrayList;
  * 3. 점수 계산해서 출력하기 
  * */
 public class Game {
+	static final int MAX_ROUND = 10;
 	ArrayList<Player> playerList;
+	int round;
 	
 	public Game() {
 		playerList = new ArrayList<>();
+		round = 1;
 	}
 	
 	public void run(int playerNum) {
@@ -21,8 +24,28 @@ public class Game {
 //			playerList.add(new Player("Player" + i));
 //		}
 		Player p = new Player("Player1");
+		
 		//2. Pin random 생성해서 출력해보기
+		Pins pins = new Pins();
+		pins.rolling();
+		
+		int firstShot = pins.getPins();
+		
+		pins.setPins();
+		pins.rolling();
+
+		int secondShot = pins.getPins();
+		
 		//3. 첫번째 frame에 쓰러뜨린 pin 기록하기
+		ArrayList<Frame> playerFrameList = p.getFrameList();
+		playerFrameList.get(round).addPinCount(firstShot);
+		playerFrameList.get(round).addPinCount(secondShot);
+		
+		ArrayList<Integer> pinCountList = playerFrameList.get(round).getPinCountList();
+		for(int count: pinCountList) {
+			System.out.println(count);
+		}
+		
 		//4. 10번 frame돌면서 기록하기 및 결과 출력하기
 		//5. 10번 돌 때, score 계산하고 출력하기
 		//6. player 수 늘려서 출력하기
--- src/game/Pins.java
+++ src/game/Pins.java
@@ -5,6 +5,7 @@ package game;
  * 1. Max pin 갯수 지정하기
  * 2. 랜덤값 생성해서 몇 개의 pin 쓰러뜨렸는지 정하기
  *    - max pin을 넘어서 생성될 수 없음
+ * 3. 처음 shot에 따라서, random값 조정
  **/
 public class Pins {
 	private static final int MAX_BOWLING_PIN_NUM = 10;
@@ -12,6 +13,10 @@ public class Pins {
 	
 	public Pins() {
 		pins = MAX_BOWLING_PIN_NUM;
+	}
+	
+	public void setPins() {
+		this.pins = MAX_BOWLING_PIN_NUM;
 	}
 	
 	public void rolling() {
